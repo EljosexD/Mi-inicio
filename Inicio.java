339a2aad62925db184379df37ef0891c9200d8f5
EljosexD <150199569+EljosexD@users.noreply.github.com>
2023-11-07 18:04:37 -0500

    Inicio 7/11/2023
    Hola :D

---
+++ Inicio.java
@@ -0,0 +1,6 @@
+public class Inicio{
+    public static void main(String[] args) {
+        System.out.println("Hola mundo , bueno este cuenta sera mi gran recorrido en el mundo de la programacion , siendo sincero estoy emocionado de apreder :D para mi primer lenguaje de programacion sera java porque java pues no se XD el profesor me recomendo esta lenguaje");
+        System.out.println("Bueno adios :D");
+    }
+}
